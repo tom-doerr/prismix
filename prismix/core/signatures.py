d197d787247106cecbcd3fdd21df59f1f73b2425
Tom Dörr <tomdoerr96@gmail.com>
2024-12-17 11:27:29 +0100

    fix: Corrected FileEdit signature description

--- prismix/core/signatures.py
+++ prismix/core/signatures.py
@@ -50,6 +50,4 @@ class FileEdit(dspy.Signature):
     instruction = dspy.InputField(desc="The instruction for the edit operation.")
     filename = dspy.OutputField(desc="The filename where the edit will be applied.")
     search = dspy.OutputField(desc="The search pattern to be replaced.")
-    replacement = dspy.OutputField(
-        desc="The replacement code."
-    )
+    replacement = dspy.OutputField(desc="The replacement code.")
