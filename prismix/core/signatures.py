b255381bb8c3e5dccd86f2e82efa5ff840f316e8
Tom Dörr <tomdoerr96@gmail.com>
2024-12-17 19:52:26 +0100

    docs: Clarify FileEdit signature documentation

--- prismix/core/signatures.py
+++ prismix/core/signatures.py
@@ -45,6 +45,7 @@ class FileEdit(dspy.Signature):
     """
     Signature for editing code in a file.
     """
+
     context = dspy.InputField(desc="The code block to be edited.")
     instruction = dspy.InputField(desc="The instruction for the edit operation.")
     filename = dspy.OutputField(desc="The filename where the edit will be applied.")
