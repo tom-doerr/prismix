fe4a3670e9fe423bfe1162da1efb6dba547752a4
Tom Dörr <tomdoerr96@gmail.com>
2024-12-22 11:55:10 +0100

    feat: Refactor CodeEdit to use dspy.Signature

--- code_edit_signature.py
+++ code_edit_signature.py
@@ -28,21 +28,24 @@ class SearchReplaceEditInstruction(BaseModel):
     search_text: str = Field(..., desc="The text to search for.")
     replacement_text: str = Field(..., desc="The text to replace the found text with.")
 
-# class EditInstructions(BaseModel):
-    # edit_instructions: list[Union[LineNumberEditInstruction, SearchReplaceEditInstruction]] = Field(..., desc="A list of edit instructions.")
+class EditInstructions(BaseModel):
+    edit_instructions: list[Union[LineNumberEditInstruction, SearchReplaceEditInstruction]] = Field(..., desc="A list of edit instructions.")
 
-# class CodeEdit(dspy.Signature):
-    # """Edits a code file based on an instruction."""
-    # instruction = dspy.InputField(desc="Instruction on how to modify the code.")
-    # code_files = dspy.InputField(desc="List of code files with their content.", format=list)
-    # context = dspy.InputField(desc="Context for the code edit.", format=Context)
-    # # edit_instructions: list[Union[LineNumberEditInstruction, SearchReplaceEditInstruction]] = Field(..., desc="A list of edit instructions.")
-    # edit_instructions = dspy.OutputField(desc="A list of edit instructions."
-    # search_query = dspy.OutputField(desc="A search query to use for the next iteration, if needed.")
+class CodeEdit(dspy.Signature):
+    """Edits a code file based on an instruction."""
+    instruction = dspy.InputField(desc="Instruction on how to modify the code.")
+    code_files = dspy.InputField(desc="List of code files with their content.", format=list)
+    context = dspy.InputField(desc="Context for the code edit.", format=Context)
+    edit_instructions: list[Union[LineNumberEditInstruction, SearchReplaceEditInstruction]] = Field(..., desc="A list of edit instructions.")
+    edit_instructions = dspy.OutputField(desc="A list of edit instructions.", format=list)
+    search_query = dspy.OutputField(desc="A search query to use for the next iteration, if needed.")
 
-class CodeEdit(BaseModel):
-    instruction: Union[LineNumberEditInstruction, SearchReplaceEditInstruction]
-    code_files: list[CodeFile]
-    context: Context
-    search_query: str = Field(None, desc="A search query to use for the next iteration, if needed.")
-    edit_instructions: list[Union[LineNumberEditInstruction, SearchReplaceEditInstruction]] = Field(None, desc="A list of edit instructions.")
+# class CodeEditPydantic(BaseModel):
+    # instruction: Union[LineNumberEditInstruction, SearchReplaceEditInstruction]
+    # code_files: list[CodeFile]
+    # context: Context
+    # search_query: str = Field(None, desc="A search query to use for the next iteration, if needed.")
+    # edit_instructions: list[Union[LineNumberEditInstruction, SearchReplaceEditInstruction]] = Field(None, desc="A list of edit instructions.")
+
+
+# CodeEdit = create_signature_class_from_model(CodeEditPydantic)
